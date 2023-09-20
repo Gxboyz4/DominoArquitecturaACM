94f2724d3a744af6dc00f861174c66bd9262c012
Gxboyz4 <125517545+Gxboyz4@users.noreply.github.com>
2023-09-19 21:14:04 -0700

    Primeros frames (frmprincipal y menú) pruebas de imágenes en maven

---
+++ ProyectoArquitecturaDominoACM/src/main/java/org/itson/proyectoarquitecturadominoacm/ProyectoArquitecturaDominoACM.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.itson.proyectoarquitecturadominoacm;
+
+/**
+ *
+ * @author Gabriel Mancinas
+ */
+public class ProyectoArquitecturaDominoACM {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
